ea9e0b1bc085d88b12985c9f3e38221b1c6decba
KazimDandir <kikbal.dandir@gmail.com>
2024-05-28 19:35:26 +0300

    The delete script was kept in a separate file and the script path was embedded in the layout, then used in the index page

---
+++ TestMVCFrameworkProject/Content/deleteUser.js
@@ -0,0 +1,17 @@
+﻿function deleteUser(userId) {
+    $.ajax({
+        url: '/User/DeleteUser',
+        type: 'POST',
+        data: { id: userId },
+        success: function (response) {
+            if (response.success) {
+                location.reload(); // Sayfayı yenilemek
+            } else {
+                alert(response.message);
+            }
+        },
+        error: function () {
+            alert('Error occurred while deleting the user.');
+        }
+    });
+}
